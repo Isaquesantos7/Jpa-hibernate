c69d90504993b012d4b0a0309a5f768743f503c2
isaquesantos7 <isaquesantos.second.1998@gmail.com>
2024-05-21 22:14:47 -0300

    feat: finding and delete with jpa hibernate

--- src/main/java/org/example/application/Program.java
+++ src/main/java/org/example/application/Program.java
@@ -15,13 +15,28 @@ public class Program {
         Person p1 = new Person(null, "Isaque", "isaquesantos.1998@gmail.com");
         Person p2 = new Person(null, "Eliseu", "elispdam.1999@gmail.com");
         Person p3 = new Person(null, "Cleidiane", "clei!123@gmail.com");
+        Person p4 = new Person(null, "Teste", "teste@gmail.com");
 
-        em.persist(p1);
-        em.persist(p2);
-        em.persist(p3);
+
+        /* Finding */
+        // Person p = em.find(Person.class, 4);
+        // System.out.println(p);
+
+        /* Remove */
+        Person p = em.find(Person.class, 4);
+        em.remove(p);
+
+        /* Inserting */
+        // em.persist(p1);
+        // em.persist(p2);
+        // em.persist(p3);
+        // em.persist(p4);
 
         em.getTransaction().begin();
         em.getTransaction().commit();
+
+        emf.close();
+        em.close();
 
         System.out.print("Pronto");
     }
